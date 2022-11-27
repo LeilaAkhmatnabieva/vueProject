4bdb06cd5c9fa04f584a6299418e28f8f7f2bd98
Лейля Ахматнабиева <l.akhmatnabieva@bars.group>
2022-11-28 02:17:52 +0300

    Убрала лишнее

--- WebAppBack/Controllers/CitizensController.cs
+++ WebAppBack/Controllers/CitizensController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using WebApplication1.Model;
@@ -13,79 +15,117 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class CitizensController : ControllerBase
     {
-        private readonly ILogger<CitizensController> _logger;
-
-        public CitizensController(ILogger<CitizensController> logger)
+        private IDbConnection CreateConnection()
         {
-            _logger = logger;
+            return new NpgsqlConnection(
+                "Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;");
         }
 
         [HttpGet("Get")]
         public List<Gilec> Get()
         {
-            List<Gilec> res;
-            using (var connection = new NpgsqlConnection("Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;"))
+            try
             {
-                res = connection.Query<Gilec>("SELECT * FROM gilec;").ToList();
+                List<Gilec> res;
+                using (var connection = CreateConnection())
+                {
+                    res = connection.Query<Gilec>("SELECT * FROM gilec;").ToList();
+                }
+                return res;
             }
-
-            return res;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+                throw;
+            }
         }
         
         [HttpGet("GetById")]
         public Gilec GetById(int id)
         {
-            
-            Gilec res;
-            using (var connection = new NpgsqlConnection("Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;"))
+            try
             {
-                res = connection.QueryFirst<Gilec>($"SELECT * FROM gilec WHERE id = {id};");
+                Gilec res;
+                using (var connection = CreateConnection())
+                {
+                    res = connection.QueryFirst<Gilec>($"SELECT * FROM gilec WHERE id = {id};");
+                }
+                return res;
             }
-            return res;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+                throw;
+            }
         }
-        [HttpGet("Delete")]
+        [HttpDelete("Delete")]
         public int Delete(int id)
         {
-            int res;
-            using (var connection = new NpgsqlConnection("Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;"))
+            try
             {
-                res = connection.Execute($"DELETE FROM gilec WHERE id = {id};");
+                int res;
+                using (var connection = CreateConnection())
+                {
+                    res = connection.Execute($"DELETE FROM gilec WHERE id = {id};");
+                }
+                return res;
             }
-            return res;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+                throw;
+            }
         }
         
         [HttpPost("Add")]
         public int Add(Gilec gilec)
         {
-            int res;
-            using (var connection = new NpgsqlConnection("Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;"))
+            try
             {
-                res = connection.Execute(@$"
-                    INSERT INTO public.gilec(
-	                first_name, middle_name, last_name, birth_date, snils, is_auto)
-	                VALUES ('{gilec.first_name??"null"}', '{gilec.middle_name??"null"}', '{gilec.last_name??"null"}', {gilec.birth_date.ToString()??"null"}, {gilec.snils.ToString()??"null"}, {gilec.is_auto.ToString()??"null"});");
+                int res;
+                using (var connection = CreateConnection())
+                {
+                    string sql = @$"
+                    INSERT INTO public.gilec(first_name, middle_name, last_name, birth_date, snils, is_auto)
+	                VALUES ('{gilec.first_name ?? "null"}', '{gilec.middle_name ?? "null"}', '{gilec.last_name ?? "null"}',
+                    {gilec.birth_date?.ToString() ?? "null"}, {gilec.snils?.ToString() ?? "null"}, {gilec.is_auto?.ToString() ?? "null"});";
+                    res = connection.Execute(sql);
+                }
+                return res;
             }
-            return res;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+                throw;
+            }
         }
         
         [HttpPost("Update")]
         public int Update(Gilec gilec)
         {
-            int res;
-            using (var connection =
-                new NpgsqlConnection(
-                    "Server = localhost; Database = citizen_app; User Id = postgres; Password = leila; Port = 5432;"))
+            try
             {
-                res = connection.Execute(@$"
+                int res;
+                using (var connection = CreateConnection())
+                {
+                    string sql = @$"
                     UPDATE public.gilec
-	                SET first_name='{gilec.first_name ?? "null"}', middle_name='{gilec.middle_name ?? "null"}',
-                        last_name='{gilec.last_name ?? "null"}', birth_date='{gilec.birth_date.ToString() ?? "null"}',
-                        snils='{gilec.snils.ToString() ?? "null"}', is_auto = '{gilec.is_auto.ToString()??"null"}'
-	                WHERE id = {gilec.id};");
+                    SET first_name = '{gilec.first_name ?? "null"}',
+                        middle_name = '{gilec.middle_name ?? "null"}',
+                        last_name = '{gilec.last_name ?? "null"}',
+                        birth_date = '{gilec.birth_date?.ToString() ?? "null"}',
+                        snils = {gilec.snils?.ToString() ?? "null"},
+                        is_auto = {gilec.is_auto?.ToString() ?? "null"}
+                    WHERE id = {gilec.id};";
+                    res = connection.Execute(sql);
+                }
+                return res;
             }
-
-            return res;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+                throw;
+            }
         }
-        
     }
 }
